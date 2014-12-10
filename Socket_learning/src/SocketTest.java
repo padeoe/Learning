fc9a4bf2e570891ac71e6b3ae72462cc963ed1cd
padeoe <padeoe@gmail.com>
2014-12-10 14:54:31 +0800

    修改，增加了服务器创建，连接服务器，输出特定域名所有IP等方法

--- Socket_learning/src/SocketTest.java
+++ Socket_learning/src/SocketTest.java
@@ -15,34 +15,103 @@ import java.util.*;
  * @version    
  *    
  */
-public class SocketTest {
 
+public class SocketTest {
 	/**
 	 * @param args
 	 * @throws IOException 
 	 * @throws UnknownHostException 
 	 */
-	public static void main(String[] args) throws UnknownHostException, IOException {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
-		InetAddress []address=InetAddress.getAllByName("baidu.com");
-		for(InetAddress e:address)
-		System.out.println(e.toString());
+		Socket_learning a=new Socket_learning();
+	    a.createServer();
+	//	a.printAllIP("dropbox.com");
+	//	a.connect_socket2("time-A.timefreq.bldrdoc.gov", 13);
 		
+	}
+
+}
+
+class Socket_learning{
+	static void connect_socket(String host,int port) throws IOException{
 		try{
 		Socket s=new Socket();
-		s.connect(new InetSocketAddress("time-A.timefreq.bldrdoc.gov",13),5000);
-			s.setSoTimeout(1000);
+		//s.connect(new InetSocketAddress("time-A.timefreq.bldrdoc.gov",13),1000);
+		s.connect(new InetSocketAddress(host,port),5000);
+		//s.setSoTimeout(5000);
 			InputStream inStream=s.getInputStream();
 			Scanner in=new Scanner(inStream);
-			
 			while(in.hasNextLine()){
 				String line=in.nextLine();
 				System.out.println(line);
 			}
+			in.close();
+			s.close();
 		}
-		catch(InterruptedIOException e){
+		catch(IOException e){
 			System.out.println("连接超时");
 		}
 	}
-
+	
+	static void connect_socket2(String host,int port) {
+		try(Socket s=new Socket()){
+		s.connect(new InetSocketAddress("time-A.timefreq.bldrdoc.gov",13),1000);
+		s.connect(new InetSocketAddress(host,port),5000);
+		//s.setSoTimeout(5000);
+			InputStream inStream=s.getInputStream();
+			Scanner in=new Scanner(inStream);
+			while(in.hasNextLine()){
+				String line=in.nextLine();
+				System.out.println(line);
+			}
+			in.close();
+			s.close();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("连接超时");
+		}
+	}
+	
+	static void printAllIP(String address){
+		InetAddress[] IP;
+		try {
+			IP = InetAddress.getAllByName(address);
+			for(InetAddress e:IP)
+				System.out.println(e.toString());
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("无法解析的域名");
+		}
+		
+	}
+	
+	static void createServer(){
+		try(ServerSocket server=new ServerSocket(8189)){
+			
+			try(Socket incoming=server.accept()){
+				InputStream inStream=incoming.getInputStream();
+			//	OutputStream outStream=new outStream("UTF-8");
+				OutputStream outStream=incoming.getOutputStream();
+				try(Scanner in=new Scanner(inStream)){		
+					PrintWriter print_to_screen=new PrintWriter(outStream,true);
+					print_to_screen.println("Hello,I'm WuKangkang,Please input anything you want)");
+					print_to_screen.println("What you have input is:   "+in.nextLine()+"  Goodbye!");
+					
+				}
+				
+			}
+			
+		}
+//		catch(){
+//			
+//		}
+ catch (IOException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+	}
+	
 }
